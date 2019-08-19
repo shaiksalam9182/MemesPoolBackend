bc4281d459ca0c0f9e35d40d9eecf02daff87063
salam <salam.shaik@tv2z.com>
2019-08-19 22:47:23 +0530

    changed images to images array

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
     post_id:String,
-    img_name:String,
+    img_name:[String],
     description:String,
     likes_count:Number,
     post_by:String,
