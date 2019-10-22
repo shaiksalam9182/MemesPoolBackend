b3a2d3e36bea40a8b6b779036de42b1cee811edf
salam <salam.shaik@tv2z.com>
2019-10-23 00:17:03 +0530

    changing uploads path

--- controllers/getImage.js
+++ controllers/getImage.js
@@ -2,7 +2,7 @@ const fs  = require('fs');
 
 exports.getImage = (req,res) =>{
     var file = req.params.name;
-    var img = fs.readFileSync('/uploads/'+file);
+    var img = fs.readFileSync('/home/shaiksalam9182/uploads/'+file);
     res.writeHead(200,{'Content-type':'image/png'});
     res.end(img,'binary');
 }
