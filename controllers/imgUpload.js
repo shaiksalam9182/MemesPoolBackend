9692bcb193940dfa47b29298b665195cdd1e41fa
shaiksalam1314 <salam.shaik@tv2z.com>
2019-12-21 20:20:25 +0530

    changed image uploading code to upload images into cloud

--- controllers/imgUpload.js
+++ controllers/imgUpload.js
@@ -2,48 +2,42 @@ const multer = require('multer');
 const fs = require('fs');
 const randomString = require('randomstring');
 const path = require('path');
+const { Storage } = require('@google-cloud/storage');
+const configFile = require('../configs/memesdev.json');
+
+exports.upload = (req, res) => {
+    const storageObject = new Storage({
+        keyFilename: path.join(__dirname, '..', 'configs', 'memesdev.json')
+    });
+
+    const bucket = storageObject.bucket('images_memespool');
+    const filename = randomString.generate(20) + path.extname(req.file.originalname);
+    const file = bucket.file(filename);
 
 
-var storage = multer.diskStorage({
-    destination:"/home/shaiksalam9182/uploads/",
-    filename: function (req, file, cb) {
-        var generatedName = randomString.generate(16) + path.extname(file.originalname);
-        console.log(generatedName);
-        cb(null, generatedName)
-    }
-})
-
-var uploads = multer({
-    storage: storage, fileFilter: function (req, file, callback) {
-        var ext = path.extname(file.originalname);
-        if (ext != ".jpg" && ext != ".jpeg" && ext != ".png") {
-            return new callback(new Error('Only Images are allowed'))
-        }
-        callback(null, true);
-    }
-}).array('uploads',15);
-
-exports.upload = (req,res)=>{
-
-    uploads(req,res,function(err){
-        if(err instanceof multer.MulterError){
-            console.log(err);
-            res.status(200).send({
-                success:false,
-                message:"This file cannot be uploaded"
-            })
-        }else if(err){
-            console.log(err);
-            res.status(200).send({
-                success:false,
-                message:"This file cannot be uploaded"
-            })
-        }else{
-            res.status(200).send({
-                success:true,
-                image: req.files
-            })
+    const stream = file.createWriteStream({
+        metadata: {
+            contentType: req.file.mimetype
         }
     })
+
+    stream.on('error', (error) => {
+        return res.status(200).send({
+            success: false,
+            message: error
+        })
+    })
+
+    stream.on('finish', () => {
+        return res.status(200).send({
+            success: true,
+            image_url: "https://storage.cloud.google.com/images_memespool/" + filename
+        })
+    })
+
+
+    stream.end(req.file.buffer);
+
+
 
 }
