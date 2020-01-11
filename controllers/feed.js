a05ae04b6ae918f6bf7c8f653df9e3bbcb24dc86
shaiksalam1314 <salam.shaik@tv2z.com>
2020-01-11 15:56:00 +0530

    modified feed api to support new db queries

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,47 +1,54 @@
-const postModel = require('../models/post');
 const tokenVerifier = require('../controllers/tokenVerifier');
+const dbConnection = require('../configs/db_connect');
 
 
-exports.getFeed = (req,res)=>{
-    if(Object.keys(req.body).length==0){
-        sendFailedResponse(req,res,"Request is empty");
-    }else if(req.body.user_id=="" || !req.body.user_id){
-        sendFailedResponse(req,res,"User id is empty")
-    }else if(req.body.token=="" || !req.body.token){
-        sendFailedResponse(req,res,"Token is empty");
-    }else if(req.body.skip==undefined ){
+exports.getFeed = (req, res) => {
+    if (Object.keys(req.body).length == 0) {
+        sendFailedResponse(req, res, "Request is empty");
+    } else if (req.body.user_id == "" || !req.body.user_id) {
+        sendFailedResponse(req, res, "User id is empty")
+    } else if (req.body.token == "" || !req.body.token) {
+        sendFailedResponse(req, res, "Token is empty");
+    } else if (req.body.skip == undefined) {
         console.log(req.body.skip);
-        sendFailedResponse(req,res,"Skip value is empty");
+        sendFailedResponse(req, res, "Skip value is empty");
     }
 
 
-    tokenVerifier.validateToken(req.body.user_id,req.body.token).then(data=>{
-        getTheFeed(req,res);
-    }).catch(err=>{
-        sendFailedResponse(req,res,err);
+    tokenVerifier.validateToken(req.body.user_id, req.body.token).then(data => {
+        getTheFeed(req, res);
+    }).catch(err => {
+        sendFailedResponse(req, res, err);
     })
 
 }
 
-function sendFailedResponse(req,res,message){
+function sendFailedResponse(req, res, message) {
     return res.status(200).send({
-        success:false,
-        message:message
+        success: false,
+        message: message
     })
 }
 
-function getTheFeed(req,res){
-    postModel.find({},{
-        _id:0,
-        __v:0,
-        likes:0
-    }).limit(10).skip(req.body.skip)
-    .then(data=>{
-        return res.status(200).send({
-            success:true,
-            data:data
+function getTheFeed(req, res) {
+    var receivedData = [];
+    dbConnection.db.collection('posts').orderBy('post_id').startAt(req.body.skip).limit(10).get()
+        .then(data => {
+            if (data) {
+                data.forEach(doc => {
+                    delete doc.data().likes;
+                    receivedData.push(doc.data());
+                })
+                return res.status(200).send({
+                    success: true,
+                    data: receivedData
+                })
+            } else {
+                sendFailedResponse(req, res, "No data found");
+            }
         })
-    }).catch(err=>{
-        sendFailedResponse(err.message);
-    })
+        .catch(err => {
+            console.log(err);
+            sendFailedResponse(req, res, err);
+        })
 }
